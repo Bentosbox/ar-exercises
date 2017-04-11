cc0ad969637c0d9ea66f64f82b02b7fadfc9e152
Ben Li <ben_li5@yahoo.ca>
2017-04-11 21:21:29 +0000

    Up to exercise 4 search and put with WHERE

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,30 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+# Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+# Richmond (annual_revenue of 1260000 carries women's apparel only)
+# Gastown (annual_revenue of 190000 carries men's apparel only)
+
+burnaby = Store.create(
+  name: "Burnaby",
+  annual_revenue: 300_000,
+  womens_apparel: true,
+  mens_apparel: true
+  )
+
+richmond = Store.create(
+  name: "Richmond",
+  annual_revenue: 1_260_000,
+  womens_apparel: true,
+  mens_apparel: false
+  )
+
+gastown = Store.create(
+  name: "Gastown",
+  annual_revenue: 190_000,
+  womens_apparel: false,
+  mens_apparel: true
+  )
+
+puts Store.count
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,41 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+# Surrey (annual_revenue of 224000, carries women's apparel only)
+# Whistler (annual_revenue of 1900000 carries men's apparel only)
+# Yaletown (annual_revenue of 430000 carries men's and women's apparel)
+
+surrey = Store.create(
+  name: "Surrey",
+  annual_revenue: 224_000,
+  womens_apparel: true,
+  mens_apparel: false
+  )
+
+whistler = Store.create(
+  name: "Burnaby",
+  annual_revenue: 1_900_000,
+  womens_apparel: false,
+  mens_apparel: true
+)
+
+yaletown = Store.create(
+  name: "Yaletown",
+  annual_revenue: 430_000,
+  womens_apparel: true,
+  mens_apparel: true
+)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |revenueOutput|
+  puts "#{revenueOutput.name}'s revenue #{revenueOutput.annual_revenue}"
+end
+
+@womens_stores = Store.where(womens_apparel: true).where('annual_revenue < 1000000')
+
+@womens_stores.each do |revenueWomen|
+  puts "#{revenueWomen.name}'s' revenue #{revenueWomen.annual_revenue}"
+end
+
+puts Store.count
